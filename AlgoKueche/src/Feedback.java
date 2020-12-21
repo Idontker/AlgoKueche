97cf44b2c68a137039ff48cf7ebb90490e77b63e
Baletiballo <Silas@kuder.org>
2020-12-21 11:14:06 +0100

    feedback

---
+++ AlgoKueche/src/Feedback.java
@@ -0,0 +1,13 @@
+public class Feedback {
+    public void arbeitschritt(String schritt){
+        //TODO
+    }
+
+    public void rezeptauswahl(String rezept){
+        //TODO
+    }
+
+	public void bewerte() {
+        //TODO
+	}
+}
