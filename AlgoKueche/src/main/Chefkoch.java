d4e5fb836825780b7738c18c39b6f1f54a25c4ea
idontker <idontker@users.noreply.github.com>
2021-01-05 17:57:41 +0100

    implemented reinerSalat in Chefkoch

--- AlgoKueche/src/main/Chefkoch.java
+++ AlgoKueche/src/main/Chefkoch.java
@@ -4,7 +4,20 @@ public class Chefkoch {
 
     private Lehrling sam;
 
+    public static void main(String args[]){
+        Chefkoch albert = new Chefkoch();
+        albert.reinerSalat();
+    }
+
     public Chefkoch() {
         sam = new Lehrling();
+    }
+
+    public void reinerSalat() {
+        sam.wirKochenJetzt("reiner Salat");
+        sam.nimmAusSchrank("einen Salat");
+        sam.schneide();
+        sam.gebeAufTeller();
+        sam.serviere();
     }
 }
