ea582dba6685b190196eacb21164276f5fa38548
idontker <idontker@users.noreply.github.com>
2021-01-08 17:40:37 +0100

    added a Construtor to slides load the images here

--- AlgoKueche/src/gui/Slide.java
+++ AlgoKueche/src/gui/Slide.java
@@ -2,7 +2,8 @@ package gui;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class Slide {
 	String method;
@@ -24,6 +25,19 @@ public class Slide {
 	Slide(String method, Color c, BufferedImage image) {
 		this(method, c);
 		this.image = image;
+	}
+
+	Slide(String method, Color c, String imageName, String comment) {
+		this(method, c, comment);
+		String pathToImage = GUI.pathToAlgoKueche + "res/" + imageName;
+		try {
+			File f = new File(pathToImage);
+			image = ImageIO.read(f);
+		} catch (Exception e) {
+			System.err.println("[ERROR]:Failed loading Image for\t" + method + "\t<" + pathToImage + ">");
+			e.printStackTrace();
+			image = null;
+		}
 	}
 
 	@Override
