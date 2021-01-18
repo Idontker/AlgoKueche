6a58e424640e68e8f2248145ceed193bf1e6bf8e
Baletiballo <Silas@kuder.org>
2021-01-18 14:55:10 +0100

    GUIU Fenstergröße passt jetzt
    MainFrame nutzt jetzt die Konstanten aus GUI, und nicht frische ints (=0)

--- AlgoKueche/src/hilfsklassen/gui/SlideMap.java
+++ AlgoKueche/src/hilfsklassen/gui/SlideMap.java
@@ -24,20 +24,34 @@ public class SlideMap extends HashMap<String, Slide> {
     }
 
     private void initSlides() {
-        this.put("welcome", new Slide("welcome", Color.cyan, "Albert.png", "Willkommen in der AlgoKueche!"));
-        this.put("wirKochenJetzt", new Slide("wirKochenJetzt", Color.blue, "sam.png", "Sam ist bereit!"));
-        this.put("nimmAusSchrank", new Slide("nimmAusSchrank", Color.cyan, "nimmAusSchrank.png", "Sam hat aus dem Schrank genommen:",true));
-        this.put("stellZutatZurueck", new Slide("stellZutatZurueck", Color.gray, "nimmAusSchrank.png", "Sam hat zurueck gestellt:",true));
-        this.put("schneide", new Slide("schneide", Color.green, "schneide.png", "Sam hat geschnitten:",true));
-        this.put("gibInTopf", new Slide("gibInTopf", Color.magenta, "gibInTopf.png", "Sam hat in den Topf geworfen:",true));
-        this.put("koche", new Slide("koche", Color.orange, "kochen.jpg", "Sam hat den Topf kochen lassen:",true));
-        this.put("serviere", new Slide("serviere", Color.yellow, "glocke.png", "Sam hat das Gericht serviert!"));
-        this.put("reactionHappy", new Slide("reactionHappy", new Color(50, 200, 10), "happy.png", "Der Kunde ist zufrieden!",true));
-        this.put("reactionSad", new Slide("reactionSad", new Color(200, 100, 50), "sad.png", "Der Kunde ist unzufrieden!",true));
-        this.put("gebeAufTeller", new Slide("gebeAufTeller", Color.pink, "teller.png", "Sam hat folgendes dem Teller beigefuegt:",true));
-        this.put("istGewuerztTrue", new Slide("istGewuerztTrue", Color.green, "abschmecken.png", "Sam hat abgeschmeckt: Es schmeckt lecker!"));
-        this.put("istGewuerztFalse", new Slide("istGewuerztFalse", Color.red, "abschmecken.png", "Sam hat abgeschmeckt: Es schmeckt nicht gut!"));
-        this.put("alert", new Slide("alert", Color.DARK_GRAY, "alert.png", "Ein Fehler ist aufgetreten: ", true));
+        this.put(       "welcome",             
+            new Slide(  "welcome",            Color.cyan,     "Albert.png",           "Willkommen in der AlgoKueche!"));
+        this.put(       "wirKochenJetzt",      
+            new Slide(  "wirKochenJetzt",     Color.blue,     "sam.png",              "Sam ist bereit!"));
+        this.put(       "nimmAusSchrank",      
+            new Slide(  "nimmAusSchrank",     Color.cyan,     "nimmAusSchrank.png",   "Sam hat aus dem Schrank genommen:",true));
+        this.put(       "stellZutatZurueck",   
+            new Slide(  "stellZutatZurueck",  Color.gray,     "nimmAusSchrank.png",   "Sam hat zurueck gestellt:",true));
+        this.put(       "schneide",            
+            new Slide(  "schneide",           Color.green,    "schneideSalat.png",         "Sam hat geschnitten:",true));
+        this.put(       "gibInTopf",           
+            new Slide(  "gibInTopf",          Color.magenta,  "gibInTopf.png",        "Sam hat in den Topf geworfen:",true));
+        this.put(       "koche",               
+            new Slide(  "koche",              Color.orange,   "kochen.jpg",           "Sam hat den Topf kochen lassen:",true));
+        this.put(       "serviere",            
+            new Slide(  "serviere",           Color.yellow,   "serviere.png",         "Sam hat das Gericht serviert!"));
+        this.put(       "reactionHappy",       
+            new Slide(  "reactionHappy",      Color.green,    "happy.png",            "Der Kunde ist zufrieden!",true));
+        this.put(       "reactionSad",         
+            new Slide(  "reactionSad",        Color.red,      "sad.png",              "Der Kunde ist unzufrieden!",true));
+        this.put(       "gebeAufTeller",       
+            new Slide(  "gebeAufTeller",      Color.pink,     "gibZutatAufTeller.png","Sam hat folgendes dem Teller beigefuegt:",true));
+        this.put(       "istGewuerztTrue",     
+            new Slide(  "istGewuerztTrue",    Color.green,    "abschmecken.png",      "Sam hat abgeschmeckt: Es schmeckt lecker!"));
+        this.put(       "istGewuerztFalse",    
+            new Slide(  "istGewuerztFalse",   Color.red,      "abschmecken.png",      "Sam hat abgeschmeckt: Es schmeckt nicht gut!"));
+        this.put(       "alert",               
+            new Slide(  "alert",              Color.DARK_GRAY, "alert.png",           "Ein Fehler ist aufgetreten: ", true));
     }
 
 }
