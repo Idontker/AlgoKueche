fab847b62caf3f2c6b2568a20e46f149f4544a81
Baletiballo <Silas@kuder.org>
2021-01-21 15:27:06 +0100

    restliche Bilder von Julia eingebaut

--- AlgoKueche/src/hilfsklassen/gui/SlideMap.java
+++ AlgoKueche/src/hilfsklassen/gui/SlideMap.java
@@ -37,7 +37,7 @@ public class SlideMap extends HashMap<String, Slide> {
         this.put(       "gibInTopf",           
             new Slide(  "gibInTopf",          Color.magenta,  "gibInTopf.png",        "Sam hat in den Topf geworfen:",true));
         this.put(       "koche",               
-            new Slide(  "koche",              Color.orange,   "kochen.jpg",           "Sam hat den Topf kochen lassen:",true));
+            new Slide(  "koche",              Color.orange,   "koche.png",           "Sam hat den Topf kochen lassen:",true));
         this.put(       "serviere",            
             new Slide(  "serviere",           Color.yellow,   "serviere.png",         "Sam hat das Gericht serviert!"));
         this.put(       "reactionHappy",       
@@ -47,15 +47,15 @@ public class SlideMap extends HashMap<String, Slide> {
         this.put(       "gebeAufTeller",       
             new Slide(  "gebeAufTeller",      Color.pink,     "gibZutatAufTeller.png","Sam hat folgendes dem Teller beigefuegt:",true));
         this.put(       "gutGewuerzt",     
-            new Slide(  "gutGewuerzt",        Color.green,    "abschmecken.png",      "Sam hat abgeschmeckt: Es schmeckt lecker!"));
+            new Slide(  "gutGewuerzt",        Color.green,    "gutGewuerzt.png",      "Sam hat abgeschmeckt: Es schmeckt lecker!"));
         this.put(       "schlechtGewuerzt",    
-            new Slide(  "schlechtGewuerzt",   Color.red,      "abschmecken.png",      "Sam hat abgeschmeckt: Es schmeckt nicht gut!"));
+            new Slide(  "schlechtGewuerzt",   Color.red,      "schlechtGewuerzt.png", "Sam hat abgeschmeckt: Es schmeckt nicht gut!"));
         this.put(       "alert",               
             new Slide(  "alert",              Color.DARK_GRAY, "alert.png",           "Ein Fehler ist aufgetreten: ", true));
         this.put(       "sumoHungrig",             
-            new Slide(  "sumoHungrig",        Color.cyan,     "sumoHungrig.png",      "Der Sumoringer ist noch hungrig."));
+            new Slide(  "sumoHungrig",        Color.red,       "sumoHungrig.png",       "Der Sumoringer ist noch hungrig."));
         this.put(       "sumoSatt",             
-            new Slide(  "sumoSatt",           Color.cyan,     "sumoSatt.png",         "Der Sumoringer ist endlich satt."));
+            new Slide(  "sumoSatt",           Color.green,     "sumoSatt.png",        "Der Sumoringer ist endlich satt."));
     }
 
 }
